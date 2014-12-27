adf2d73cad03ca5a783701a67284cc78842860b0
earth <earth@aqua.media.teu.ac.jp>
2014-12-27 20:38:39 +0900

    12/27

---
+++ CLI/FK_Integration/CLI/Capsule_CLI.h
@@ -0,0 +1,72 @@
+﻿// Block_CLI.h
+
+#pragma once
+
+#include <FK/Capsule.h>
+#include "IndexFace_CLI.h"
+#include "Vector_CLI.h"
+
+namespace FK_CLI
+{
+	using namespace std;
+	using namespace System;
+
+	public ref class fk_Capsule : FK_CLI::fk_IndexFaceSet {
+	internal:
+		::fk_Capsule * GetP(void)
+		{
+			return (::fk_Capsule *)(pBase);
+		}
+
+	public:
+		fk_Capsule::fk_Capsule() : fk_IndexFaceSet(false)
+		{
+			pBase = new ::fk_Capsule();
+		}
+
+		fk_Capsule::fk_Capsule(bool argNewFlg) : fk_IndexFaceSet(false)
+		{
+			if(argNewFlg == true) {
+				pBase = new ::fk_Capsule();
+			}
+				
+		}
+
+		fk_Capsule::fk_Capsule(int argDiv, double argLen, double argRad)
+			: fk_IndexFaceSet(false)
+		{
+			pBase = new ::fk_Capsule(argDiv, argLen, argRad);
+		}
+
+		fk_Capsule::~fk_Capsule()
+		{
+			this->!fk_Capsule();
+		}
+
+		fk_Capsule::!fk_Capsule()
+		{
+			if(dFlg == true) delete pBase;
+			pBase = NULL;
+		}
+
+		void setDivide(int argDiv)
+		{
+			GetP()->setDivide(argDiv);
+		}
+
+		void setLength(double argLen)
+		{
+			GetP()->setLength(argLen);
+		}
+
+		void setRadius(double argRad)
+		{
+			GetP()->setRadius(argRad);
+		}
+
+		void setScale(double argScale)
+		{
+			GetP()->setScale(argScale);
+		}
+	};
+}
---
+++ CLI/FK_Integration/CLI/cpp/Capsule_CLI.cpp
@@ -0,0 +1,1 @@
+﻿#include "../Capsule_CLI.h"
