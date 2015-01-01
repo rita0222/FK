1468d237540ca6ebfc9bbbe985111ffc2a6b872e
earth <earth@aqua.media.teu.ac.jp>
2015-01-01 16:40:26 +0900

    1/1

---
+++ CLI/Test/FK_CLI_Sprite/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FK_CLI;
+
+namespace FK_CLI_Sprite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var window = new fk_AppWindow();
+			fk_Material.initDefault();
+			
+			var sprite = new fk_SpriteModel();
+			var block = new fk_Block(1.0, 1.0, 1.0);
+			var model = new fk_Model();
+			var origin = new fk_Vector(0.0, 0.0, 0.0);
+			int count;
+			string str;
+ 
+			if(sprite.initFont("mona.ttf") == false) {
+				System.Console.WriteLine("Font Error");
+			}
+
+			sprite.setPositionLT(-280.0, 230.0);
+			window.entry(sprite);
+ 
+			model.setShape(block);
+			model.glMoveTo(0.0, 6.0, 0.0);
+			model.setMaterial(fk_Material.Yellow);
+			window.entry(model);
+ 
+			window.setCameraPos(0.0, 5.0, 20.0);
+			window.setCameraFocus(0.0, 5.0, 0.0);
+			window.setSize(800, 600);
+			window.setBGColor(0.6, 0.7, 0.8);
+			window.open();
+			window.showGuide(fk_GuideMode.GRID_XZ);
+ 
+			count = 0;
+			while(window.update() == true) {
+				str = "count = " + count.ToString();
+				sprite.drawText(str, true);
+				model.glRotateWithVec(origin, fk_Axis.Y, FK.PI/360.0);
+				count++;
+			}
+		}
+	}
+}
---
+++ CLI/Test/FK_CLI_TextImage/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FK_CLI;
+
+namespace FK_CLI_TextImage
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// ウィンドウ生成
+			var win = new fk_AppWindow();
+			fk_Material.initDefault();
+
+			var textImage = new fk_TextImage();
+			var texture = new fk_RectTexture();
+			var str = new fk_UniStr();
+			var strModel = new fk_Model();
+
+			str.convert("FK 日本語");
+			texture.setImage(textImage);
+			if(textImage.initFont("mona.ttf") == false) {
+				System.Console.WriteLine("Font Error");
+			}
+			textImage.setDPI(96);
+			textImage.setPTSize(96);
+			textImage.setLineSkip(30);
+			textImage.setForeColor(0.5, 1.0, 0.8, 1.0);
+			textImage.setBackColor(0.2, 0.7, 0.8, 0.0);
+			textImage.setAlign(fk_TextAlign.CENTER);
+			textImage.loadUniStr(str);
+			texture.setTextureSize(40.0, 10.0);
+			strModel.setMaterial(fk_Material.TrueWhite);			
+
+			strModel.setShape(texture);
+			strModel.glVec(0.0, 0.0, -1.0);
+			win.entry(strModel);
+			strModel.glRotateWithVec(0.0, 0.0, 0.0, fk_Axis.X, FK.PI/2.0);
+			win.open();
+			win.setCameraPos(0.0, 0.0, 100.0);
+			win.setCameraFocus(0.0, 0.0, 0.0);
+			while(win.update() == true) {
+				strModel.glRotateWithVec(0.0, 0.0, 0.0, fk_Axis.X, -FK.PI/500.0);
+			}
+		}
+	}
+}
