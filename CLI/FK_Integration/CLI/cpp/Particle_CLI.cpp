80e31fb812ad5ca09eaa8281780baa4d6bcbd9c6
earth <earth@aqua.media.teu.ac.jp>
2015-01-03 14:09:01 +0900

    1/3

---
+++ CLI/FK_Integration/CLI/cpp/Particle_CLI.cpp
@@ -0,0 +1,94 @@
+﻿#include "Particle_CLI.h"
+
+namespace FK_CLI {
+
+	using namespace std;
+
+	void fk_Particle::init(void)
+	{
+		GetP()->init();
+	}
+
+	int fk_Particle::getID(void)
+	{
+		return GetP()->getID();
+	}
+
+	unsigned int fk_Particle::getCount(void)
+	{
+		return GetP()->getCount();
+	}
+
+	fk_Vector^ fk_Particle::getPosition(void)
+	{
+		return gcnew fk_Vector(GetP()->getPosition());
+	}
+
+	void fk_Particle::setPosition(fk_Vector^ argPos)
+	{
+		if(!argPos) return;
+		GetP()->setPosition(*argPos->pVec);
+	}
+
+	void fk_Particle::setPosition(double x, double y, double z)
+	{
+		GetP()->setPosition(x, y, z);
+	}
+
+	fk_Vector^ fk_Particle::getVelocity(void)
+	{
+		return gcnew fk_Vector(GetP()->getVelocity());
+	}
+
+	void fk_Particle::setVelocity(fk_Vector^ argVel)
+	{
+		if(!argVel) return;
+		GetP()->setVelocity(*argVel->pVec);
+	}
+
+	void fk_Particle::setVelocity(double argX, double argY, double argZ)
+	{
+		GetP()->setVelocity(argX, argY, argZ);
+	}
+
+	fk_Vector^ fk_Particle::getAccel(void)
+	{
+		return gcnew fk_Vector(GetP()->getAccel());
+	}
+
+	void fk_Particle::setAccel(fk_Vector^ argAcc)
+	{
+		if(!argAcc) return;
+		GetP()->setAccel(*argAcc->pVec);
+	}
+
+	void fk_Particle::setAccel(double argX, double argY, double argZ)
+	{
+		GetP()->setAccel(argX, argY, argZ);
+	}
+
+	int fk_Particle::getColorID(void)
+	{
+		return GetP()->getColorID();
+	}
+
+	void fk_Particle::setColorID(int argID)
+	{
+		GetP()->setColorID(argID);
+	}
+
+	bool fk_Particle::getDrawMode(void)
+	{
+		return GetP()->getDrawMode();
+	}
+									  
+	void fk_Particle::setDrawMode(bool argMode)
+	{
+		GetP()->setDrawMode(argMode);
+	}
+
+	void fk_Particle::handle(void)
+	{
+		GetP()->handle();
+	}
+}
---
+++ CLI/FK_Integration/CLI/include/Particle_CLI.h
@@ -0,0 +1,54 @@
+﻿// Particle_CLI.h
+
+#pragma once
+
+#include <FK/Particle.h>
+#include "Attribute_CLI.h"
+#include "Vector_CLI.h"
+
+namespace FK_CLI
+{
+	public ref class fk_Particle : fk_Attribute {
+	internal:
+		::fk_Particle * GetP(void)
+		{
+			return (::fk_Particle *)(pBase);
+		}
+
+	public:
+		fk_Particle::fk_Particle(bool argNewFlg) : fk_Attribute(false)
+		{
+		}
+
+		fk_Particle::fk_Particle() : fk_Attribute(false)
+		{
+		}
+
+		fk_Particle::~fk_Particle()
+		{
+			this->!fk_Particle();
+		}
+
+		fk_Particle::!fk_Particle()
+		{
+		}
+
+		void init(void);
+		int getID(void);
+		unsigned int getCount(void);
+		fk_Vector^ getPosition(void);
+		void setPosition(fk_Vector^ pos);
+		void setPosition(double x, double y, double z);
+		fk_Vector^ getVelocity(void);
+		void setVelocity(fk_Vector^ vel);
+		void setVelocity(double x, double y, double z);
+		fk_Vector^ getAccel(void);
+		void setAccel(fk_Vector^ acc);
+		void setAccel(double x, double y, double z);
+		int getColorID(void);
+		void setColorID(int ID);
+		bool getDrawMode(void);
+		void setDrawMode(bool mode);
+		void handle(void);
+	};
+}
