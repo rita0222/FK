725cf1c9f6c4122990bf6cd9cd4945619cd94d44
earth <earth@gamescience.jp>
2015-01-15 16:04:22 +0900

    1/15

---
+++ CLI/Test/FK_CLI_Boid/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FK_CLI;
+
+namespace FK_CLI_Boid
+{
+	class Boid {
+		private fk_Model [] agent;
+		private fk_Cone cone;
+		private const int IAREA = 15;
+		private const double AREASIZE = (double)(IAREA);
+
+		private double paramA, paramB, paramC, paramLA, paramLB;
+
+		public Boid(int argNum)
+		{
+			fk_Material.initDefault();
+			cone = new fk_Cone(16, 0.4, 1.0);
+			if(argNum < 0) return;
+			agent = new fk_Model[argNum];
+			var rand = new Random();
+			for(int i = 0; i < argNum; ++i) {
+				agent[i] = new fk_Model();
+				agent[i].setShape(cone);
+				agent[i].setMaterial(fk_Material.Red);
+				agent[i].glVec(rand.NextDouble()*2.0 - 1.0,
+					rand.NextDouble()*2.0 - 1.0, 0.0);
+				agent[i].glMoveTo(rand.NextDouble() * AREASIZE * 2.0 - AREASIZE,
+					rand.NextDouble() * AREASIZE * 2.0 - AREASIZE, 0.0);
+			}
+			paramA = 0.2;
+			paramB = 0.02;
+			paramC = 0.01;
+			paramLA = 3.0;
+			paramLB = 5.0;
+		}
+
+		public void setParam(double argA, double argB, double argC,
+			double argLA, double argLB)
+		{
+			paramA = argA;
+			paramB = argB;
+			paramC = argC;
+			paramLA = argLA;
+			paramLB = argLB;
+		}
+
+		public void setWindow(fk_AppWindow argWin)
+		{
+			for(int i = 0; i < agent.Length; ++i) {
+				argWin.entry(agent[i]);
+			}
+		}
+
+		public void forward()
+		{
+			int i, j;
+			var gVec = new fk_Vector();
+			var pArray = new fk_Vector[agent.Length];
+			var vArray = new fk_Vector[agent.Length];
+			fk_Vector diff;
+			
+			for(i = 0; i < agent.Length; ++i) {
+				pArray[i] = agent[i].getPosition();
+				vArray[i] = agent[i].getVec();
+				gVec += pArray[i];
+			}
+
+			gVec /= (double)(agent.Length);
+
+			for(i = 0; i < agent.Length; ++i) {
+				var vec = agent[i].getVec();
+				for(j = 0; j < agent.Length; ++j) {
+					if(i == j) continue;
+
+					diff = pArray[i] - pArray[j];
+					double dist = diff.dist();
+					if(dist < paramLA) {
+						vec += paramA * diff / (dist*dist);
+					}
+
+					if(dist < paramLB) {
+						vec += paramB * vArray[j];
+					}
+				}
+				vec += paramC * (gVec - pArray[i]);
+
+				if((pArray[i].x > AREASIZE && vArray[i].x > 0.0) ||
+					(pArray[i].x < -AREASIZE && vArray[i].x < 0.0)) {
+					vec.x -= vec.x * 0.1;
+				}
+
+				if((pArray[i].y > AREASIZE && vArray[i].y > 0.0) ||
+					(pArray[i].y < -AREASIZE && vArray[i].y < 0.0)) {
+					vec.y -= vec.y * 0.1;
+				}
+
+				vec.z = 0.0;
+
+				agent[i].glVec(vec);
+				agent[i].loTranslate(0.0, 0.0, -0.05);
+			}
+		}
+	}
+
+	class Program {
+		static void Main(string[] args)
+		{
+			var win = new fk_AppWindow();
+			var boid = new Boid(50);
+
+			boid.setWindow(win);
+
+			win.setSize(600, 600);
+			win.setBGColor(0.6, 0.7, 0.8);
+			win.showGuide(fk_GuideMode.GRID_XY);
+			win.setCameraPos(0.0, 0.0, 80.0);
+			win.setCameraFocus(0.0, 0.0, 0.0);
+
+			win.open();
+
+			while(win.update() == true) {
+				boid.forward();
+			}
+
+		}
+	}
+}
