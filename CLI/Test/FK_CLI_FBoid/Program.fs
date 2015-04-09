9a479225359e6ac65bb76b0a1e8f84b6d6792570
earth <earth@gamescience.jp>
2015-04-09 21:19:14 +0900

    150409

---
+++ CLI/Test/FK_CLI_FBoid/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open FK_CLI
+
+type Agent = class
+    val mutable model : fk_Model
+    val mutable newVec : fk_Vector
+end;;
+
+module FK_Boid =
+    let win = new fk_AppWindow()
+    fk_Material.InitDefault()
+
+    win.Open()
+    let i = ref 0
+    while win.Update() = true do
+        incr i
+        
