33b70b24038ae2006d448a5afe0e6b4e722a3cd7
earth <earth@gamescience.jp>
2015-04-24 17:53:51 +0900

    150424

--- CLI/FK_Integration/CLI/include/Base_CLI.h
+++ CLI/FK_Integration/CLI/include/Base_CLI.h
@@ -7,73 +7,35 @@
 namespace FK_CLI {
 	using namespace System;
 
+	//! リテラル管理クラス
+	/*!
+	 *	このクラスは、FK 内で用いるリテラル(固定値)を管理します。
+	 */
 	public ref class FK {
 	public:
-		literal double PI = _FK_PI_DEFINE_;
-		literal double EPS = _FK_EPS_DEFINE_;
+		literal double PI = _FK_PI_DEFINE_;		//!< 円周率
+		literal double EPS = _FK_EPS_DEFINE_;	//!< 数値演算用誤差値
 	};
 
-	public enum class fk_ObjectType {
-		BASEOBJECT,
-		MATRIXADMIN,
-		MODEL,
-		DISPLAYLINK,
-		SCENE,
-		SHAPE,
-		POLYGON,
-		LINE,
-		POLYLINE,
-		POINT,
-		CIRCLE,
-		SPHERE,
-		BLOCK,
-		CLOSEDLINE,
-		PRISM,
-		CAPSULE,
-		CONE,
-		INDEXFACESET,
-		LIGHT,
-		BEZCURVE,
-		BSPLCURVE,
-		BEZSURFACE,
-		BSPLSURFACE,
-		IMAGE,
-		TEXCOORD,
-		TEXTURE,
-		RECTTEXTURE,
-		TRITEXTURE,
-		MESHTEXTURE,
-		IFSTEXTURE,
-		UNICHAR,
-		UNISTR,
-		TEXTIMAGE,
-		COLOR,
-		MATERIAL,
-		PALETTE,
-		PLANE,
-		WINDOW,
-		PICKDATA,
-		PROJECTBASE,
-		PERSPECTIVE,
-		FRUSTUM,
-		ORTHO,
-		VERTEX,
-		HALF,
-		EDGE,
-		LOOP,
-		SOLID,
-		SHAPEVIEWER,
-		PARTICLE,
-		PARTICLESET
-	};
-
+	//! FK の各クラスの基盤となる基本クラス
+	/*!
+	 *	このクラスは、FKのユーザが利用する大半のクラスの基底クラスとなっています。
+	 */
 	public ref class fk_BaseObject {
 	public:
+#ifndef FK_DOXYGEN_USER_PROCESS		
 		bool dFlg;
 		::fk_BaseObject *pBase;
-
+#endif
+		//! コンストラクタ
 		fk_BaseObject(bool);
+
+		//! デストラクタ
 		~fk_BaseObject();
+
+		//! ファイナライザ
 		!fk_BaseObject();
 	};
 }
+
+
