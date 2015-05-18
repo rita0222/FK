9350c062cff6323721b069cfe220beb8eec232cd
rita <s2rita0222@gmail.com>
2015-05-19 01:42:48 +0900

    進捗が見えるようにバッチファイルを修正

--- build_vs/2013/lib_build.bat
+++ build_vs/2013/lib_build.bat
@@ -2,38 +2,38 @@
 echo --- Build solutions ---
 
 ::  VS2013Ç÷ÇÃÉpÉXÇê›íË
-set VS12_DEVENV_PATH=%VS120COMNTOOLS%..\IDE\devenv.exe
+set VS12_DEVENV_PATH=%VS120COMNTOOLS%..\IDE\devenv
 
-echo FK
+:: FK
 "%VS12_DEVENV_PATH%" FK.sln /Build "Debug|Win32"
 "%VS12_DEVENV_PATH%" FK.sln /Build "Debug|x64"
 "%VS12_DEVENV_PATH%" FK.sln /Build "Release|Win32"
 "%VS12_DEVENV_PATH%" FK.sln /Build "Release|x64"
 
-echo FLTK
+:: FLTK
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Build "Debug|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Build "Debug|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Build "Release|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Build "Release|x64"
 
-echo freetype
+:: freetype
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Build "Debug Multithreaded|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Build "Debug Multithreaded|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Build "Release Multithreaded|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Build "Release Multithreaded|x64"
 
-echo libiconv
+:: libiconv
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Build "DebugStatic|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Build "DebugStatic|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Build "ReleaseStatic|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Build "ReleaseStatic|x64"
 
-echo ogg
+:: ogg
 "%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\libogg_dynamic.sln /Build "Release|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\libogg_dynamic.sln /Build "Release|x64"
 
-echo vorbis
-"%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\vorbis_dynamic.sln /Build "Release|Win32"
-"%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\vorbis_dynamic.sln /Build "Release|x64"
+:: vorbis
+"%VS12_DEVENV_PATH%" ..\extlibs\libvorbis\win32\VS2013\vorbis_dynamic.sln /Build "Release|Win32"
+"%VS12_DEVENV_PATH%" ..\extlibs\libvorbis\win32\VS2013\vorbis_dynamic.sln /Build "Release|x64"
 
 echo Done!
--- build_vs/2013/lib_clean.bat
+++ build_vs/2013/lib_clean.bat
@@ -2,38 +2,38 @@
 echo --- Clean solutions ---
 
 ::  VS2013Ç÷ÇÃÉpÉXÇê›íË
-set VS12_DEVENV_PATH=%VS120COMNTOOLS%..\IDE\devenv.exe
+set VS12_DEVENV_PATH=%VS120COMNTOOLS%..\IDE\devenv
 
-echo FK
+:: FK
 "%VS12_DEVENV_PATH%" FK.sln /Clean "Debug|Win32"
 "%VS12_DEVENV_PATH%" FK.sln /Clean "Debug|x64"
 "%VS12_DEVENV_PATH%" FK.sln /Clean "Release|Win32"
 "%VS12_DEVENV_PATH%" FK.sln /Clean "Release|x64"
 
-echo FLTK
+:: FLTK
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Clean "Debug|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Clean "Debug|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Clean "Release|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\fltk-1.3.2\ide\VisualC2013\fltk.sln /Clean "Release|x64"
 
-echo freetype
+:: freetype
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Clean "Debug Multithreaded|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Clean "Debug Multithreaded|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Clean "Release Multithreaded|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\freetype-2.5.3\builds\windows\vc2013\freetype.sln /Clean "Release Multithreaded|x64"
 
-echo libiconv
+:: libiconv
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Clean "DebugStatic|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Clean "DebugStatic|x64"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Clean "ReleaseStatic|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2013.sln /Clean "ReleaseStatic|x64"
 
-echo ogg
+:: ogg
 "%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\libogg_dynamic.sln /Clean "Release|Win32"
 "%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\libogg_dynamic.sln /Clean "Release|x64"
 
-echo vorbis
-"%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\vorbis_dynamic.sln /Clean "Release|Win32"
-"%VS12_DEVENV_PATH%" ..\extlibs\libogg\win32\VS2013\vorbis_dynamic.sln /Clean "Release|x64"
+:: vorbis
+"%VS12_DEVENV_PATH%" ..\extlibs\libvorbis\win32\VS2013\vorbis_dynamic.sln /Clean "Release|Win32"
+"%VS12_DEVENV_PATH%" ..\extlibs\libvorbis\win32\VS2013\vorbis_dynamic.sln /Clean "Release|x64"
 
 echo Done!
