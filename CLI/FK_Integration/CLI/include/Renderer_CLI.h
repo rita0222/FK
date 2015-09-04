a8b88f4bd4624dd51c9b4602a4519a1a01dd77de
earth <earth@gamescience.jp>
2015-09-04 14:10:13 +0900

    150904

--- CLI/FK_Integration/CLI/include/Renderer_CLI.h
+++ CLI/FK_Integration/CLI/include/Renderer_CLI.h
@@ -1,3 +1,34 @@
+﻿// Renderer_CLI.h
+
+#pragma once
+
+#include <FK/Engine.H>
+#include <msclr/marshal_cppstd.h>
+#include "Scene_CLI.h"
+
+namespace FK_CLI
+{
+	public ref class fk_Renderer {
+	private:
+		HWND	hWnd;
+		HDC		hDC;
+		HGLRC	hRC;
+
+		::fk_GraphicsEngine *pEngine;
+
+	public:
+		fk_Renderer::fk_Renderer(void);
+		fk_Renderer::~fk_Renderer();
+		fk_Renderer::!fk_Renderer();
+
+		bool Initialize(System::IntPtr pWnd, int w, int h);
+		void Shutdown(void);
+		void Resize(int w, int h);
+		void SetScene(fk_Scene^ scene);
+		void Draw(void);
+	};
+}
+
 /****************************************************************************
  *
  *	Copyright (c) 1999-2015, Fine Kernel Project, All rights reserved.
@@ -69,34 +100,3 @@
  *	ついて、一切責任を負わないものとします。
  *
  ****************************************************************************/
-
-﻿// Renderer_CLI.h
-
-#pragma once
-
-#include <FK/Engine.H>
-#include <msclr/marshal_cppstd.h>
-#include "Scene_CLI.h"
-
-namespace FK_CLI
-{
-	public ref class fk_Renderer {
-	private:
-		HWND	hWnd;
-		HDC		hDC;
-		HGLRC	hRC;
-
-		::fk_GraphicsEngine *pEngine;
-
-	public:
-		fk_Renderer::fk_Renderer(void);
-		fk_Renderer::~fk_Renderer();
-		fk_Renderer::!fk_Renderer();
-
-		bool Initialize(System::IntPtr pWnd, int w, int h);
-		void Shutdown(void);
-		void Resize(int w, int h);
-		void SetScene(fk_Scene^ scene);
-		void Draw(void);
-	};
-}
