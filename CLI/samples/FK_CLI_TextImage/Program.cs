d0854ef438f5d1d1de8df1c89faac1d05b24a119
earth <earth@gamescience.jp>
2016-01-01 14:00:30 +0900

    160101

--- CLI/samples/FK_CLI_TextImage/Program.cs
+++ CLI/samples/FK_CLI_TextImage/Program.cs
@@ -12,7 +12,6 @@ namespace FK_CLI_TextImage
 		static void Main(string[] args)
 		{
 			// ウィンドウ生成
-
 			var win = new fk_AppWindow();
 			fk_Material.InitDefault();
 
@@ -21,30 +20,64 @@ namespace FK_CLI_TextImage
 			var str = new fk_UniStr();
 			var strModel = new fk_Model();
 
-			str.Convert("FK日本語");
-			texture.Image = textImage;
-			if(textImage.InitFont("mona.ttf") == false) {
+            // 文字列生成
+            str.Convert("FK日本語");
+
+            // フォントデータ入力
+            if(textImage.InitFont("rm1b.ttf") == false) {
 				System.Console.WriteLine("Font Error");
 			}
-			textImage.DPI = 96;
-			textImage.PTSize = 96;
-			textImage.LineSkip = 30;
-			textImage.ForeColor = new fk_Color(0.5, 1.0, 0.8, 1.0);
-			textImage.BackColor = new fk_Color(0.2, 0.7, 0.8, 0.0);
-			textImage.Align = fk_TextAlign.CENTER;
-			textImage.LoadUniStr(str);
-			texture.TextureSize = new fk_TexCoord(40.0, 10.0);
-			strModel.Material = fk_Material.TrueWhite;
 
+            // テクスチャに文字列板を設定
+            texture.Image = textImage;
+
+            // 一文字あたりのピクセル数は DPI * PTSize となる
+            textImage.DPI = 96;
+            textImage.PTSize = 96;
+
+            // 文字色
+			textImage.ForeColor = new fk_Color(0.5, 1.0, 0.8, 1.0);
+
+            // 背景色
+            textImage.BackColor = new fk_Color(0.2, 0.7, 0.8, 0.0);
+
+            // 整列 (今回は中央揃え)
+            textImage.Align = fk_TextAlign.CENTER;
+
+            // 等幅モード設定 (本来はデフォルトで true)
+            textImage.MonospaceMode = true;
+
+            // 等幅モード時の横幅ピクセルサイズ (PTSizeと同一がよい)
+            textImage.MonospaceSize = 96;
+
+            // 文字列を入力し文字列板を生成
+            textImage.LoadUniStr(str);
+
+            // テクスチャサイズの設定
+            texture.TextureSize = new fk_TexCoord(40.0, 10.0);
+
+            // 文字列板モデルのマテリアル設定
+            strModel.Material = fk_Material.TrueWhite;
+
+            // 文字列板モデルの形状を texture に設定
 			strModel.Shape = texture;
-			strModel.GlVec(0.0, 0.0, -1.0);
+
+            // 姿勢制御
+            strModel.GlVec(0.0, 0.0, -1.0);
 			strModel.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.X, FK.PI/2.0);
-			win.Entry(strModel); 
+
+            // モデル登録とウィンドウ生成
+            win.Entry(strModel); 
 			win.Open();
-			win.CameraPos = new fk_Vector(0.0, 0.0, 100.0);
-			win.CameraFocus = new fk_Vector(0.0, 0.0, 0.0);
+
+            // カメラ設定
+            win.CameraPos = new fk_Vector(0.0, 0.0, 100.0);
+            win.CameraFocus = new fk_Vector(0.0, 0.0, 0.0);
+
+            // メインループ
 			while(win.Update() == true) {
-				strModel.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.X, -FK.PI/500.0);
+                // 文字列板モデル回転
+                strModel.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.X, -FK.PI/500.0);
 			}
 		}
 	}
