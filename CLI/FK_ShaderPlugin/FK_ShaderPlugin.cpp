204d3c26b5ce46a32e7f4cd93f5c9d78dcbf14e1
rita <s2rita0222@gmail.com>
2016-05-06 07:50:55 +0900

    fk_ShaderProgram実装開始

--- CLI/FK_ShaderPlugin/FK_ShaderPlugin.cpp
+++ CLI/FK_ShaderPlugin/FK_ShaderPlugin.cpp
@@ -1,4 +1,44 @@
 // これは メイン DLL ファイルです。
 
 #include "FK_ShaderPlugin.h"
+#include "GL/glew.h"
+#include <string>
+#include <vcclr.h>
+
+namespace FK_ShaderPlugin
+{
+	bool fk_ShaderModel::Initialize()
+	{
+		if (!isExtensionInitialized)
+		{
+			isExtensionInitialized = glewInit() == GLEW_OK;
+		}
+
+		return isExtensionInitialized;
+	}
+
+	void MarshalString(String ^ s, std::string& os) {
+		using namespace Runtime::InteropServices;
+		const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+		os = chars;
+		Marshal::FreeHGlobal(IntPtr((void*)chars));
+	}
+
+	System::UInt32 fk_ShaderModel::Compile(System::String^ code, System::UInt32 type)
+	{
+		std::string nCode;
+		MarshalString(code, nCode);
+		const char *pCode = nCode.c_str();
+		System::UInt32 id = glCreateShader(type);
+		glShaderSource(id, 1, &pCode, nullptr);
+		glCompileShader(id);
+
+		return id;
+	}
+
+	fk_ShaderModel::fk_ShaderModel()
+	{
+		Initialize();
+	}
+}
 
--- CLI/FK_ShaderPlugin/FK_ShaderPlugin.h
+++ CLI/FK_ShaderPlugin/FK_ShaderPlugin.h
@@ -2,18 +2,24 @@
 
 #pragma once
 
-#include <FK/Model.h>
-
 using namespace System;
-using namespace FK_CLI;
 
 namespace FK_ShaderPlugin
 {
-	public ref class fk_ShaderModel : public FK_CLI::fk_Model
+	public ref class fk_ShaderModel
 	{
 	public:
-		fk_ShaderModel::fk_ShaderModel() : fk_Model()
-		{
-		};
+		fk_ShaderModel::fk_ShaderModel();
+		System::UInt32 Compile(System::String^ code, System::UInt32 type);
+		bool Validate();
+		bool SetUniform(System::String^ );
+		System::String^ GetError();
+
+	private:
+		System::UInt32 idProgram;
+		System::UInt32 idVertex;
+		System::UInt32 idFragment;
+		static bool	isExtensionInitialized = false;
+		static bool Initialize();
 	};
 }
