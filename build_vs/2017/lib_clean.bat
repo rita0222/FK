c5e55f60aaaa0b3c2fe160ab79cd374fbd4cf76a
rita <s2rita@nifty.com>
2017-08-31 01:00:22 +0900

    VS2017ビルド環境再調整

--- build_vs/2017/lib_clean.bat
+++ build_vs/2017/lib_clean.bat
@@ -1,43 +1,44 @@
 @echo off
 echo --- Clean solutions ---
 
-::  VS2015へのパスを設定
-set VS14_DEVENV_PATH=%VS140COMNTOOLS%..\IDE\devenv
+::  VS2017へのパスを設定
+FOR /f "DELIMS=" %%A IN ('vswhere -format value -property installationPath') DO SET VS150COMNTOOLS=%%A
+set VS15_DEVENV_PATH=%VS150COMNTOOLS%\Common7\IDE\devenv
 
 :: FK
-"%VS14_DEVENV_PATH%" FK.sln /Clean "Debug|Win32"
-"%VS14_DEVENV_PATH%" FK.sln /Clean "Debug|x64"
-"%VS14_DEVENV_PATH%" FK.sln /Clean "Release|Win32"
-"%VS14_DEVENV_PATH%" FK.sln /Clean "Release|x64"
+"%VS15_DEVENV_PATH%" FK.sln /Clean "Debug|x86"
+"%VS15_DEVENV_PATH%" FK.sln /Clean "Debug|x64"
+"%VS15_DEVENV_PATH%" FK.sln /Clean "Release|x86"
+"%VS15_DEVENV_PATH%" FK.sln /Clean "Release|x64"
 
 :: FLTK
-"%VS14_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2015\fltk.sln /Clean "Debug|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2015\fltk.sln /Clean "Debug|x64"
-"%VS14_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2015\fltk.sln /Clean "Release|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2015\fltk.sln /Clean "Release|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2017\fltk.sln /Clean "Debug|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2017\fltk.sln /Clean "Debug|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2017\fltk.sln /Clean "Release|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\fltk-1.3.3\ide\VisualC2017\fltk.sln /Clean "Release|x64"
 
 :: freetype
-"%VS14_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2015\freetype.sln /Clean "Debug Multithreaded|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2015\freetype.sln /Clean "Debug Multithreaded|x64"
-"%VS14_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2015\freetype.sln /Clean "Release Multithreaded|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2015\freetype.sln /Clean "Release Multithreaded|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2017\freetype.sln /Clean "Debug Multithreaded|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2017\freetype.sln /Clean "Debug Multithreaded|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2017\freetype.sln /Clean "Release Multithreaded|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\freetype-2.6\builds\windows\vc2017\freetype.sln /Clean "Release Multithreaded|x64"
 
 :: libiconv
-"%VS14_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2015.sln /Clean "DebugStatic|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2015.sln /Clean "DebugStatic|x64"
-"%VS14_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2015.sln /Clean "ReleaseStatic|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2015.sln /Clean "ReleaseStatic|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2017.sln /Clean "DebugStatic|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2017.sln /Clean "DebugStatic|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2017.sln /Clean "ReleaseStatic|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libiconv\LibIconv_2017.sln /Clean "ReleaseStatic|x64"
 
 :: ogg
-"%VS14_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2015\libogg_static.sln /Clean "Debug|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2015\libogg_static.sln /Clean "Debug|x64"
-"%VS14_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2015\libogg_static.sln /Clean "Release|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2015\libogg_static.sln /Clean "Release|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2017\libogg_static.sln /Clean "Debug|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2017\libogg_static.sln /Clean "Debug|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2017\libogg_static.sln /Clean "Release|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libogg-1.3.2\win32\VS2017\libogg_static.sln /Clean "Release|x64"
 
 :: vorbis
-"%VS14_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2015\vorbis_static.sln /Clean "Debug|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2015\vorbis_static.sln /Clean "Debug|x64"
-"%VS14_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2015\vorbis_static.sln /Clean "Release|Win32"
-"%VS14_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2015\vorbis_static.sln /Clean "Release|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2017\vorbis_static.sln /Clean "Debug|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2017\vorbis_static.sln /Clean "Debug|x64"
+"%VS15_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2017\vorbis_static.sln /Clean "Release|x86"
+"%VS15_DEVENV_PATH%" ..\extlibs\libvorbis-1.3.5\win32\VS2017\vorbis_static.sln /Clean "Release|x64"
 
 echo Done!
