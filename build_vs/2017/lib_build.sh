5ff5c16f000bbef2b1acef44a58ae46c08336a7d
earth <earth@gamescience.jp>
2017-11-10 15:43:32 +0900

    171110

--- build_vs/2017/lib_build.sh
+++ build_vs/2017/lib_build.sh
@@ -3,40 +3,40 @@
 source ./vsenv.sh
 
 # VS2015へのパスを設定
-export VS14_DEVENV_PATH=$VS140COMNTOOLS/../IDE/devenv
+#export VS15_DEVENV_PATH=$VS150COMNTOOLS/../IDE/devenv
 
-# FK
-"$VS14_DEVENV_PATH" FK.sln /Build "Debug|Win32"
-"$VS14_DEVENV_PATH" FK.sln /Build "Debug|x64"
-"$VS14_DEVENV_PATH" FK.sln /Build "Release|Win32"
-"$VS14_DEVENV_PATH" FK.sln /Build "Release|x64"
+## FK
+"$VS15_DEVENV_PATH" FK.sln /Build "Debug|x86"
+"$VS15_DEVENV_PATH" FK.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" FK.sln /Build "Release|x86"
+"$VS15_DEVENV_PATH" FK.sln /Build "Release|x64"
 
 # FLTK
-"$VS14_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2015\\fltk.sln /Build "Debug|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2015\\fltk.sln /Build "Debug|x64"
-"$VS14_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2015\\fltk.sln /Build "Release|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2015\\fltk.sln /Build "Release|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2017\\fltk.sln /Build "Debug|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2017\\fltk.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2017\\fltk.sln /Build "Release|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.3\\ide\\VisualC2017\\fltk.sln /Build "Release|x64"
 
 # freetype
-"$VS14_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2015\\freetype.sln /Build "Debug Multithreaded|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2015\\freetype.sln /Build "Debug Multithreaded|x64"
-"$VS14_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2015\\freetype.sln /Build "Release Multithreaded|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2015\\freetype.sln /Build "Release Multithreaded|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Build "Debug Multithreaded|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Build "Debug Multithreaded|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Build "Release Multithreaded|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Build "Release Multithreaded|x64"
 
 # libiconv
-"$VS14_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2015.sln /Build "DebugStatic|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2015.sln /Build "DebugStatic|x64"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2015.sln /Build "ReleaseStatic|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2015.sln /Build "ReleaseStatic|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "DebugStatic|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "DebugStatic|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "ReleaseStatic|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "ReleaseStatic|x64"
 
 # ogg
-"$VS14_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2015\\libogg_static.sln /Build "Debug|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2015\\libogg_static.sln /Build "Debug|x64"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2015\\libogg_static.sln /Build "Release|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2015\\libogg_static.sln /Build "Release|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Build "Debug|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Build "Release|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Build "Release|x64"
 
 # vorbis
-"$VS14_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2015\\vorbis_static.sln /Build "Debug|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2015\\vorbis_static.sln /Build "Debug|x64"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2015\\vorbis_static.sln /Build "Release|Win32"
-"$VS14_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2015\\vorbis_static.sln /Build "Release|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Build "Debug|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Build "Release|x86"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Build "Release|x64"
--- build_vs/2017/lib_installer.sh
+++ build_vs/2017/lib_installer.sh
@@ -3,7 +3,7 @@
 cd ../FK_ProjectWizard
 
 if [ -z "$ZSH_NAME" ] ; then
-	/cygdrive/c/Program\ Files/CreateInstall\ Free\ JP/ci-free.exe FK_VC15.ci
+	cygstart /cygdrive/c/Program\ Files\ \(x86\)/CreateInstall\ Free/ci-free.exe FK_VC17.ci
 else
-	cygstart FK_VC15.ci
+	cygstart FK_VC17.ci
 fi
