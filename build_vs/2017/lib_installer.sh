03c42f0eb5065ce99fb10c25c41c9cc5b540ac83
earth <earth@gamescience.jp>
2017-11-21 12:28:12 +0900

    171121

--- build_vs/2017/lib_installer.sh
+++ build_vs/2017/lib_installer.sh
@@ -3,7 +3,7 @@
 cd ../FK_ProjectWizard
 
 if [ -z "$ZSH_NAME" ] ; then
-	cygstart /cygdrive/c/Program\ Files\ \(x86\)/CreateInstall\ Free/ci-free.exe FK_VC17.ci
+	cygstart /cygdrive/c/Program\ Files/CreateInstall\ Free\ JP/ci-free.exe FK_VC17.ci
 else
 	cygstart FK_VC17.ci
 fi
