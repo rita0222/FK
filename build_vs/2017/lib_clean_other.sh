8078b6fa6df7e11a72093f49b12c8cfd5c4687ea
earth <earth@gamescience.jp>
2019-03-13 20:47:56 +0900

    190313

--- build_vs/2017/lib_clean_other.sh
+++ build_vs/2017/lib_clean_other.sh
@@ -4,15 +4,20 @@ source ./vsenv.sh
 
 # FLTK
 "$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.5\\ide\\VisualC2017\\fltk.sln /Clean "Release|x64"
+#"$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.5\\ide\\VisualC2017\\fltk.sln /Clean "Debug|x64"
 
 # freetype
 "$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Clean "Release Multithreaded|x64"
+#"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.6\\builds\\windows\\vc2017\\freetype.sln /Clean "Debug Multithreaded|x64"
 
 # libiconv
 "$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Clean "ReleaseStatic|x64"
+#"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Clean "DebugStatic|x64"
 
 # ogg
 "$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Clean "Release|x64"
+#"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.2\\win32\\VS2017\\libogg_static.sln /Clean "Debug|x64"
 
 # vorbis
 "$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Clean "Release|x64"
+#"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.5\\win32\\VS2017\\vorbis_static.sln /Clean "Debug|x64"
--- build_vs/2017/lib_copy.sh
+++ build_vs/2017/lib_copy.sh
@@ -1,43 +1,41 @@
 #!/bin/sh
 
-rm -rf ../FK_VC17
+FKPATH="../Installer/FK_VC17"
 
-mkdir ../FK_VC17
-mkdir ../FK_VC17/bin
-#mkdir ../FK_VC17/bin_x64
-mkdir ../FK_VC17/doc
-mkdir ../FK_VC17/include
-mkdir ../FK_VC17/lib
-#mkdir ../FK_VC17/lib_x64
-mkdir ../FK_VC17/redist
+rm -rf $FKPATH
 
-cp lib_x64/*.lib ../FK_VC17/lib
-cp ../extlibs/fltk-1.3.5/lib/*.lib ../FK_VC17/lib
-mv ../FK_VC17/lib/fltkzlib.lib ../FK_VC17/lib/fltkz.lib
-cp ../extlibs/freetype-2.6/objs/vc2017/x64/freetype26MT.lib ../FK_VC17/lib/freetype2MT.lib
-cp ../extlibs/libiconv/ReleaseStatic_x64/libiconv_2017Static.lib ../FK_VC17/lib/libiconv.lib
-cp ../extlibs/libogg-1.3.2/win32/VS2017/bin/x64/Release/libogg_static.lib ../FK_VC17/lib/libogg.lib
-cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbis_static.lib ../FK_VC17/lib/libvorbis.lib
-cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbisfile_static.lib ../FK_VC17/lib/libvorbisfile.lib
-cp ../extlibs/OpenAL/lib_x64/*.lib ../FK_VC17/lib
-cp ../extlibs/glew-2.1.0/lib/Release/x64/*.lib ../FK_VC17/lib
-cp ../extlibs/glew-2.1.0/bin/Release/x64/*.* ../FK_VC17/bin
+mkdir $FKPATH
+mkdir $FKPATH/bin
+mkdir $FKPATH/doc
+mkdir $FKPATH/include
+mkdir $FKPATH/lib
+mkdir $FKPATH/redist
 
-mkdir ../FK_VC17/include/FK
-cp ../../FK/*.[h,H] ../FK_VC17/include/FK
+cp lib_x64/*.lib $FKPATH/lib
+cp ../extlibs/fltk-1.3.5/lib/*.lib $FKPATH/lib
+mv $FKPATH/lib/fltkzlib.lib $FKPATH/lib/fltkz.lib
+cp ../extlibs/freetype-2.6/objs/vc2017/x64/freetype26MT.lib $FKPATH/lib/freetype2MT.lib
+cp ../extlibs/libiconv/ReleaseStatic_x64/libiconv_2017Static.lib $FKPATH/lib/libiconv.lib
+cp ../extlibs/libogg-1.3.2/win32/VS2017/bin/x64/Release/libogg_static.lib $FKPATH/lib/libogg.lib
+cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbis_static.lib $FKPATH/lib/libvorbis.lib
+cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbisfile_static.lib $FKPATH/lib/libvorbisfile.lib
+cp ../extlibs/OpenAL/lib_x64/*.lib $FKPATH/lib
+cp ../extlibs/glew-2.1.0/lib/Release/x64/*.lib $FKPATH/lib
+cp ../extlibs/glew-2.1.0/bin/Release/x64/*.* $FKPATH/bin
 
-mkdir ../FK_VC17/include/FL
-cp ../extlibs/fltk-1.3.5/FL/*.[h,H] ../FK_VC17/include/FL
+mkdir $FKPATH/include/FK
+cp ../../FK/*.[h,H] $FKPATH/include/FK
 
-mkdir ../FK_VC17/include/GL
-cp ../extlibs/glew-2.1.0/include/GL/*.h ../FK_VC17/include/GL
+mkdir $FKPATH/include/FL
+cp ../extlibs/fltk-1.3.5/FL/*.[h,H] $FKPATH/include/FL
 
-cp ../extlibs/OpenAL/redist/*.exe ../FK_VC17/redist
+mkdir $FKPATH/include/GL
+cp ../extlibs/glew-2.1.0/include/GL/*.h $FKPATH/include/GL
 
-mkdir ../FK_VC17/doc/glew
-cp ../extlibs/glew-2.1.0/LICENSE.txt ../FK_VC17/doc/glew
-cp ../doc/License.txt ../FK_VC17/doc
-cp ../doc/RelativeLibraries.txt ../FK_VC17/doc
-cp ../doc/Version_2017.txt ../FK_VC17/doc/Version.txt
+cp ../extlibs/OpenAL/redist/*.exe $FKPATH/redist
 
-cp -r vs_wizard ../FK_VC17
+mkdir $FKPATH/doc/glew
+cp ../extlibs/glew-2.1.0/LICENSE.txt $FKPATH/doc/glew
+cp ../doc/License.txt $FKPATH/doc
+cp ../doc/RelativeLibraries.txt $FKPATH/doc
+cp ../doc/Version_2017.txt $FKPATH/doc/Version.txt
