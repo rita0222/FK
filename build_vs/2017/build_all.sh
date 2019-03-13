bbaa57ee0bb5dfbde48dbd5449e1451da9832514
earth <earth@gamescience.jp>
2019-03-13 21:33:37 +0900

    190313

--- build_vs/2017/build_all.sh
+++ build_vs/2017/build_all.sh
@@ -8,10 +8,10 @@ sh -x lib_build_FK.sh
 sh -x lib_build_other.sh
 sh -x lib_copy.sh
 
-if [ -z "$ZSH_NAME" ] ; then
-	sh -x lib_installer.sh
-else
-	zsh -x lib_installer.sh
-fi
+#if [ -z "$ZSH_NAME" ] ; then
+#	sh -x lib_installer.sh
+#else
+#	zsh -x lib_installer.sh
+#fi
 
 echo "Completed!"
