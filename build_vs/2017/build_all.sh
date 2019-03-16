52d078c50b18f8334d2a01a4a586e2078ca63773
earth <earth@gamescience.jp>
2019-03-17 00:49:31 +0900

    190317

--- build_vs/2017/build_all.sh
+++ build_vs/2017/build_all.sh
@@ -2,10 +2,10 @@
 
 echo "--- Create FK Package ---"
 
-sh -x lib_clean_FK.sh
 sh -x lib_clean_other.sh
-sh -x lib_build_FK.sh
+sh -x lib_clean_FK.sh
 sh -x lib_build_other.sh
+sh -x lib_build_FK.sh
 sh -x lib_copy.sh
 
 #if [ -z "$ZSH_NAME" ] ; then
