27644f9e09831c9503d2142ba754d746b12930a1
earth <earth@gamescience.jp>
2019-03-17 19:42:49 +0900

    190317

--- build_vs/2017/lib_build_other.sh
+++ build_vs/2017/lib_build_other.sh
@@ -7,17 +7,17 @@ source ./vsenv.sh
 "$VS15_DEVENV_PATH" ..\\extlibs\\fltk-1.3.5\\ide\\VisualC2017\\fltk.sln /Build "Debug|x64"
 
 # freetype
-#"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.10.0\\builds\\windows\\vc2017\\freetype.sln /Build "Release Static|x64"
-#"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.10.0\\builds\\windows\\vc2017\\freetype.sln /Build "Debug Static|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.10.0\\builds\\windows\\vc2017\\freetype.sln /Build "Release Static|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\freetype-2.10.0\\builds\\windows\\vc2017\\freetype.sln /Build "Debug Static|x64"
 
 # libiconv
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "ReleaseStatic|x64"
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "DebugStatic|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "ReleaseStatic|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libiconv\\LibIconv_2017.sln /Build "DebugStatic|x64"
 
 # ogg
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.3\\win32\\VS2017\\libogg_static.sln /Build "Release|x64"
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.3\\win32\\VS2017\\libogg_static.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.3\\win32\\VS2017\\libogg_static.sln /Build "Release|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libogg-1.3.3\\win32\\VS2017\\libogg_static.sln /Build "Debug|x64"
 
 # vorbis
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.6\\win32\\VS2017\\vorbis_static.sln /Build "Release|x64"
-#"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.6\\win32\\VS2017\\vorbis_static.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.6\\win32\\VS2017\\vorbis_static.sln /Build "Release|x64"
+"$VS15_DEVENV_PATH" ..\\extlibs\\libvorbis-1.3.6\\win32\\VS2017\\vorbis_static.sln /Build "Debug|x64"
--- build_vs/2017/lib_copy.sh
+++ build_vs/2017/lib_copy.sh
@@ -13,23 +13,43 @@ mkdir $FKPATH/lib/release
 mkdir $FKPATH/lib/debug
 mkdir $FKPATH/redist
 
-cp lib_x64/*.lib $FKPATH/lib
+# for FK
+cp lib/release/*.lib $FKPATH/lib/release
+cp lib/debug/*.lib $FKPATH/lib/debug
 
 # for FLTK
 cp ../extlibs/fltk-1.3.5/lib/release/*.lib $FKPATH/lib/release
 cp ../extlibs/fltk-1.3.5/lib/debug/*.lib $FKPATH/lib/debug
 
 # for freetype
-cp ../extlibs/freetype-2.6/objs/vc2017/x64/freetype26MT.lib $FKPATH/lib/freetype2MT.lib
+cp ../extlibs/freetype-2.10.0/lib/Release\ Static/freetype.lib $FKPATH/lib/release/freetype.lib
+cp ../extlibs/freetype-2.10.0/lib/Debug\ Static/freetype.lib $FKPATH/lib/debug/freetype_d.lib
 
+# for iconv
+cp ../extlibs/libiconv/ReleaseStatic_x64/libiconv_2017Static.lib $FKPATH/lib/release/libiconv.lib
+cp ../extlibs/libiconv/DebugStatic_x64/libiconv_2017StaticD.lib $FKPATH/lib/debug/libiconv_d.lib
 
-cp ../extlibs/libiconv/ReleaseStatic_x64/libiconv_2017Static.lib $FKPATH/lib/libiconv.lib
-cp ../extlibs/libogg-1.3.2/win32/VS2017/bin/x64/Release/libogg_static.lib $FKPATH/lib/libogg.lib
-cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbis_static.lib $FKPATH/lib/libvorbis.lib
-cp ../extlibs/libvorbis-1.3.5/win32/VS2017/bin/x64/Release/libvorbisfile_static.lib $FKPATH/lib/libvorbisfile.lib
-cp ../extlibs/OpenAL/lib_x64/*.lib $FKPATH/lib
-cp ../extlibs/glew-2.1.0/lib/Release/x64/*.lib $FKPATH/lib
-cp ../extlibs/glew-2.1.0/bin/Release/x64/*.* $FKPATH/bin
+# for ogg
+cp ../extlibs/libogg-1.3.3/lib/release/libogg.lib $FKPATH/lib/release
+cp ../extlibs/libogg-1.3.3/lib/debug/libogg_d.lib $FKPATH/lib/debug
+
+# for vorbis
+cp ../extlibs/libvorbis-1.3.6/lib/release/libvorbis_static.lib $FKPATH/lib/release/libvorbis.lib
+cp ../extlibs/libvorbis-1.3.6/lib/release/libvorbisfile.lib $FKPATH/lib/release
+cp ../extlibs/libvorbis-1.3.6/lib/debug/libvorbis_static_d.lib $FKPATH/lib/debug/libvorbis_d.lib
+cp ../extlibs/libvorbis-1.3.6/lib/debug/libvorbisfile_d.lib $FKPATH/lib/debug
+
+# for OpenAL
+cp ../extlibs/OpenAL/lib_x64/OpenAL32.lib $FKPATH/lib/release
+cp ../extlibs/OpenAL/lib_x64/OpenAL32.lib $FKPATH/lib/debug
+
+# for glew
+cp ../extlibs/glew-2.1.0/lib/Release/x64/*.lib $FKPATH/lib/release
+cp ../extlibs/glew-2.1.0/lib/Release/x64/*.lib $FKPATH/lib/debug
+cp ../extlibs/glew-2.1.0/bin/Release/x64/glew32.dll $FKPATH/bin
+cp ../extlibs/glew-2.1.0/bin/Release/x64/glewinfo.exe $FKPATH/bin
+cp ../extlibs/glew-2.1.0/bin/Release/x64/visualinfo.exe $FKPATH/bin
+
 
 mkdir $FKPATH/include/FK
 cp ../../FK/*.[h,H] $FKPATH/include/FK
