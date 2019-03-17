64545232bc90985d7383d065f7ce0394057a3ead
earth <earth@gamescience.jp>
2019-03-17 14:48:13 +0900

    190317

--- build_vs/2017/lib_build_FK.sh
+++ build_vs/2017/lib_build_FK.sh
@@ -3,7 +3,15 @@
 source ./vsenv.sh
 
 ## FK
+
+pushd ../../src/shader/GLSL
+./glsl2out *.glsl
+popd
+pushd ../../src/graphics/GLSL
+./glsl2out *.glsl
+popd
+
 "$VS15_DEVENV_PATH" FK.sln /Build "Release|x64"
-#"$VS15_DEVENV_PATH" FK.sln /Build "Debug|x64"
+"$VS15_DEVENV_PATH" FK.sln /Build "Debug|x64"
 #"$VS15_DEVENV_PATH" FK.sln /Build "Debug|x86"
 #"$VS15_DEVENV_PATH" FK.sln /Build "Release|x86"
