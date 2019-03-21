d32cfb46aa23c36c9ce8079e92a1b24f13a67ef9
earth <earth@gamescience.jp>
2019-03-21 16:47:52 +0900

    190321

---
+++ MacPack/app_cp.zsh
@@ -0,0 +1,37 @@
+#!/usr/bin/env zsh
+
+rm -rf Package
+
+mkdir Package
+mkdir Package/FK_Lib4
+mkdir Package/FK_Lib4/include
+mkdir Package/FK_Lib4/lib
+
+cp -r include/FL Package/FK_Lib4/include
+cp -r etc Package/FK_Lib4
+cp -r doc Package/FK_Lib4
+cp -r lib/dynamic Package/FK_Lib4/lib
+cp -r lib/static Package/FK_Lib4/lib
+cp ../lib/dynamic/*.dylib Package/FK_Lib4/lib/dynamic
+cp ../lib/static/*.a Package/FK_Lib4/lib/static
+
+ranlib Package/FK_Lib4/lib/static/*.a
+
+cp -r ../FK Package/FK_Lib4/include
+
+pkgbuild \
+	--root ./Package \
+	--identifier jp.gamescience.fk \
+	--install-location /Library \
+	--version 4.0.0 \
+	--ownership preserve \
+	--scripts ./script \
+	tmpInst.pkg
+
+productbuild \
+	--distribution ./script/distribution.xml \
+	--package-path . \
+	fk_Installer.pkg
+
+rm tmpInst.pkg
+rm -rf Package
