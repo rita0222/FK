9c5177fee755d8915a2830f411773a410e9db9e0
earth <earth@gamescience.jp>
2019-04-03 19:54:00 +0900

    190403

--- CLI/Test/FK_CLI_STest1/Resources/fs1.glsl
+++
@@ -1,8 +0,0 @@
-uniform sampler2D tex;
-varying vec4 pos;
-varying vec2 fUV;
-void main(void)
-{
-    gl_FragColor.rgb = texture2D(tex, fUV).rgb;
-    gl_FragColor.a = 1.0;
-}
--- CLI/Test/FK_CLI_STest1/Resources/vs2.glsl
+++
@@ -1,6 +0,0 @@
-varying vec2 fUV;
-void main(void)
-{
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-    fUV = gl_MultiTexCoord0.xy;
-}
