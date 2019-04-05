71fe769ad04f80f7bc9bed29ebfa427b830bbe3b
earth <earth@gamescience.jp>
2019-04-05 18:25:20 +0900

    190405

--- CLI/Test/FK_CLI_Viewer/Properties/AssemblyInfo.cs
+++ CLI/Test/FK_CLI_Viewer/Properties/AssemblyInfo.cs
@@ -8,28 +8,28 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FK_CLI_Viewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("UNITCOM PC")]
 [assembly: AssemblyProduct("FK_CLI_Viewer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © UNITCOM PC 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
 // その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
 
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid("9c9d18df-daa6-42e4-8edd-32e3ce04700e")]
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("665570f8-dfbe-4b7e-936c-b899420550fb")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
 //      Revision
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- CLI/Test/FK_CLI_Viewer/Viewer.cs
+++ CLI/Test/FK_CLI_Viewer/Viewer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FK_CLI;
 
 namespace FK_CLI_Viewer
@@ -11,43 +7,45 @@ namespace FK_CLI_Viewer
 	{
 		static void Main(string[] args)
 		{
+            const int SIZE = 40;
+
 			var viewer = new fk_ShapeViewer(600, 600);
 			var shape = new fk_IndexFaceSet();
-			var pos = new fk_Vector[121];
+			var pos = new fk_Vector[(SIZE+1)*(SIZE+1)];
 			var moveVec = new fk_Vector();
 			var movePos = new fk_Vector();
-			var IFSet = new int[4*100];
+			var IFSet = new int[4*SIZE*SIZE];
 			int i, j;
 			double x, y;
 
-			for(i = 0; i <= 10; ++i) {
-				for(j = 0; j <= 10; ++j) {
-					x = (double)(i-5);
-					y = (double)(j-5);
-					pos[i*11+j] = new fk_Vector(x, y, (x*x - y*y)/10.0);
+			for(i = 0; i <= SIZE; ++i) {
+				for(j = 0; j <= SIZE; ++j) {
+					x = (double)(i - SIZE/2);
+					y = (double)(j - SIZE/2);
+					pos[i*(SIZE+1)+j] = new fk_Vector(x, y, (x*x - y*y)/(double)SIZE);
 				}
 			}
 
-			for(i = 0; i < 10; i++) {
-				for(j = 0; j < 10; j++) {
-					IFSet[(i*10 + j)*4 + 0] = i*11 + j;
-					IFSet[(i*10 + j)*4 + 1] = (i+1)*11 + j;
-					IFSet[(i*10 + j)*4 + 2] = (i+1)*11 + j+1;
-					IFSet[(i*10 + j)*4 + 3] = i*11 + j+1;
+			for(i = 0; i < SIZE; i++) {
+				for(j = 0; j < SIZE; j++) {
+					IFSet[(i*SIZE + j)*4 + 0] = i*(SIZE+1) + j;
+					IFSet[(i*SIZE + j)*4 + 1] = (i+1)*(SIZE+1) + j;
+					IFSet[(i*SIZE + j)*4 + 2] = (i+1)*(SIZE+1) + j+1;
+					IFSet[(i*SIZE + j)*4 + 3] = i*(SIZE+1) + j+1;
 				}
 			}
 
-			shape.MakeIFSet(100, 4, IFSet, 121, pos);
+			shape.MakeIFSet(SIZE*SIZE, 4, IFSet, (SIZE+1)*(SIZE+1), pos);
 			viewer.Shape = shape;
 			viewer.DrawMode = fk_DrawMode.FRONTBACK_POLYMODE | fk_DrawMode.LINEMODE;
 			viewer.Scale = 10.0;
 
 			for(int counter = 0; viewer.Draw() == true; counter += 10) {
-				for(i = 0; i <= 10; i++) {
-					for(j = 0; j <= 10; j++) {
+				for(i = 0; i <= SIZE; i++) {
+					for(j = 0; j <= SIZE; j++) {
 						moveVec.Set(0.0, 0.0, Math.Sin((double)(counter + j*40)*0.05/Math.PI));
-						movePos = moveVec + pos[i*11+j];
-						shape.MoveVPosition(i*11+j, movePos);
+						movePos = moveVec + pos[i*(SIZE+1)+j];
+						shape.MoveVPosition(i*(SIZE+1)+j, movePos);
 					}
 				}
 			}
