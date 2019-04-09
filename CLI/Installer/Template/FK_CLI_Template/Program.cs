1a8175a3c50a4794055fcb96a94a3b4735dfe774
earth <earth@gamescience.jp>
2019-04-09 13:23:43 +0900

    190409

---
+++ CLI/Installer/Template/FK_CLI_Template/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FK_CLI;
+
+namespace FK_CLI_Template
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			fk_Material.InitDefault();
+
+			// ウィンドウ生成
+			var window = new fk_AppWindow();
+            window.Size = new fk_Dimension(600, 600);
+            window.CameraPos = new fk_Vector(0.0, 0.0, 500.0);
+            window.CameraFocus = new fk_Vector(0.0, 0.0, 0.0);
+
+			// 直方体モデル生成
+			var blockModel = new fk_Model();
+			var block = new fk_Block(50.0, 70.0, 40.0);
+			blockModel.Shape = block;
+			blockModel.Material = fk_Material.Yellow;
+			window.Entry(blockModel);
+			window.Open();
+
+			while(window.Update())
+            {
+				blockModel.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.Y, Math.PI/300.0);
+			}
+		}
+	}
+}
--- CLI/Installer/cli_copy.sh
+++ CLI/Installer/cli_copy.sh
@@ -2,16 +2,27 @@
 
 source ./vsenv.sh
 
-OUTPATH="../Installer/FK_CLI"
-INPATH="../../CLI/FK_CLI_DLL"
+OUTPATH="./FK_CLI"
+INPATH="../FK_CLI_DLL"
 
 rm -rf $OUTPATH
-mkdir -p $OUtPATH/bin/x86
+mkdir -p $OUTPATH/bin/x86
 mkdir -p $OUTPATH/bin/x64
 mkdir $OUTPATH/doc
 mkdir $OUTPATH/redist
-cp $INPATH/Win32/$CLIPATH/bin/FK_CLI.dll
-cp ../doc/License.txt $CLIPATH/cddoc
-cp ../doc/Version_CLI.txt $CLIPATH/doc/Version.txt
-cp ../extlibs/OpenAL/redist/*.exe $CLIPATH/redist
-cp "$VS150COMNTOOLS\\VC\\Redist\\MSVC\\14.16.27012\\vcredist_x64.exe" $CLIPATH/redist
+cp $INPATH/Win32/Release/FK_CLI.dll $OUTPATH/bin/x86
+cp $INPATH/Win32/Release/FK_FormHelper.dll $OUTPATH/bin/x86
+cp $INPATH/Win32/Debug/FK_CLI_d.dll $OUTPATH/bin/x86
+cp $INPATH/Win32/Debug/FK_CLI_d.pdb $OUTPATH/bin/x86
+cp $INPATH/Win32/Debug/FK_FormHelper.dll $OUTPATH/bin/x86/FK_FormHelper_d.dll
+cp $INPATH/x64/Release/FK_CLI.dll $OUTPATH/bin/x64
+cp $INPATH/x64/Release/FK_FormHelper.dll $OUTPATH/bin/x64
+cp $INPATH/x64/Debug/FK_CLI_d.dll $OUTPATH/bin/x64
+cp $INPATH/x64/Debug/FK_CLI_d.pdb $OUTPATH/bin/x64
+cp $INPATH/x64/Debug/FK_FormHelper.dll $OUTPATH/bin/x64/FK_FormHelper_d.dll
+
+cp ./doc/License.txt $OUTPATH/doc
+cp ./doc/Version_CLI.txt $OUTPATH/doc/Version.txt
+cp ../../build_vs/extlibs/OpenAL/redist/*.exe $OUTPATH/redist
+cp "$VS150COMNTOOLS\\VC\\Redist\\MSVC\\14.16.27012\\vcredist_x86.exe" $OUTPATH/redist
+cp "$VS150COMNTOOLS\\VC\\Redist\\MSVC\\14.16.27012\\vcredist_x64.exe" $OUTPATH/redist
