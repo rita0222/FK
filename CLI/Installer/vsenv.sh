c45975e279008bca40748880f5548a545e4de6cb
earth <earth@gamescience.jp>
2019-04-09 11:40:07 +0900

    190409

---
+++ CLI/Installer/vsenv.sh
@@ -0,0 +1,3 @@
+export VS15VSWHERE=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/Installer/vswhere
+export VS150COMNTOOLS=`"${VS15VSWHERE}" -format value -property installationPath | sed -e "s/\r//g" | sed -e "s/C:\\\\\/\/cygdrive\/c\//" | sed -e "s/\\\\\/\//g"`
+export VS15_DEVENV_PATH="${VS150COMNTOOLS}/Common7/IDE/devenv.com"
