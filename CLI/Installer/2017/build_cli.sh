a3d387b4876e278d184092f4ac1e002dfc08cef4
earth <earth@gamescience.jp>
2019-09-12 13:00:13 +0900

    190912

--- CLI/Installer/2017/build_cli.sh
+++ CLI/Installer/2017/build_cli.sh
@@ -5,9 +5,3 @@ echo "--- Create FK CLI Package ---"
 sh -x cli_clean.sh
 sh -x cli_build.sh
 sh -x cli_copy.sh
-
-#if [ -z "$ZSH_NAME" ] ; then
-#	sh -x cli_installer.sh
-#else
-#	zsh -x cli_installer.sh
-#fi
--- CLI/Installer/2017/cli_copy.sh
+++ CLI/Installer/2017/cli_copy.sh
@@ -3,7 +3,7 @@
 source ./vsenv.sh
 
 OUTPATH="./FK_CLI"
-INPATH="../FK_CLI_DLL"
+INPATH="../../2017/FK_CLI_DLL"
 
 rm -rf $OUTPATH
 mkdir -p $OUTPATH/bin/x86
@@ -19,7 +19,7 @@ cp $INPATH/x64/Release/FK_FormHelper.dll $OUTPATH/bin/x64
 
 cp ./doc/License.txt $OUTPATH/doc
 cp ./doc/Version_CLI.txt $OUTPATH/doc/Version.txt
-cp ../contrib/bin/x86/* $OUTPATH/dll/x86
-cp ../contrib/bin/x64/* $OUTPATH/dll/x64
+cp ../../contrib/bin/x86/* $OUTPATH/dll/x86
+cp ../../contrib/bin/x64/* $OUTPATH/dll/x64
 cp "$VS150COMNTOOLS\\VC\\Redist\\MSVC\\14.16.27012\\vcredist_x86.exe" $OUTPATH/redist
 cp "$VS150COMNTOOLS\\VC\\Redist\\MSVC\\14.16.27012\\vcredist_x64.exe" $OUTPATH/redist
