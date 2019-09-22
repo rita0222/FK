13487823afcf30eb11f39582dc0a159d419ea5e4
earth <earth@gamescience.jp>
2019-09-22 18:34:18 +0900

    190922

--- CLI/2019/Test/FK_CLI_Box/Box.cs
+++ CLI/2019/Test/FK_CLI_Box/Box.cs
@@ -7,19 +7,17 @@ namespace FK_CLI_Box
 	{
 		static void Main(string[] args)
 		{
-			int i;
+            // マテリアル情報の初期化
+            fk_Material.InitDefault();
 
-			// ウィンドウ生成
-			var win = new fk_AppWindow();
-			fk_Material.InitDefault();
+            // ウィンドウ生成
+            var win = new fk_AppWindow();
 
-			// 光源生成
-			var light = new fk_Light();
-			var lightModel = new fk_Model();
-			lightModel.Material = fk_Material.TrueWhite;
-			lightModel.GlMoveTo(0.0, 0.0, 0.0);
-			lightModel.GlFocus(-1.0, -1.0, -1.0);
-			win.Entry(lightModel);
+            // ウィンドウサイズ設定
+            win.Size = new fk_Dimension(600, 600);
+
+            // Frame Per Second (画面更新速度) の設定
+            win.FPS = 60;
 
 			// 直方体モデル生成
 			var blockModel = new fk_Model();
@@ -36,7 +34,7 @@ namespace FK_CLI_Box
 			pos[3] = -pos[1];
 			fk_Line[] line = new fk_Line[2];
 			fk_Model[] lineModel = new fk_Model[2];
-			for(i = 0; i < 2; i++) {
+			for(int i = 0; i < 2; i++) {
 				line[i] = new fk_Line();
 				line[i].PushLine(pos[2*i], pos[2*i + 1]);
 				lineModel[i] = new fk_Model();
@@ -44,11 +42,10 @@ namespace FK_CLI_Box
 				lineModel[i].Parent = blockModel;
 				win.Entry(lineModel[i]);
 			}
-
 			lineModel[0].LineColor = new fk_Color(1.0, 0.0, 0.0);
 			lineModel[1].LineColor = new fk_Color(0.0, 1.0, 0.0);
 
-			// カメラモデル生成
+			// カメラモデル設定
 			var camera = new fk_Model();
 			camera.GlMoveTo(0.0, 0.0, 2000.0);
 			camera.GlFocus(0.0, 0.0, 0.0);
@@ -58,12 +55,18 @@ namespace FK_CLI_Box
 
 			var origin = new fk_Vector(0.0, 0.0, 0.0);
 
-			for(i = 0; win.Update() == true; i++) {
+			for(int count = 0; win.Update() == true; count++) {
+                // カメラ前進
 				camera.GlTranslate(0.0, 0.0, -1.0);
-				blockModel.GlRotateWithVec(origin, fk_Axis.Y, Math.PI/300.0);
-				var cPos = camera.Position;
-				if(cPos.z < -0.00001) camera.GlFocus(origin);
-				if(i >= 1000) camera.LoRotateWithVec(origin, fk_Axis.Z, Math.PI/500.0);
+
+                // ブロックを y 軸中心に回転
+                blockModel.GlRotateWithVec(origin, fk_Axis.Y, Math.PI/300.0);
+
+                // カメラの注視点を原点に向ける
+				camera.GlFocus(origin);
+
+                // カウンターが1000を上回ったらカメラをz軸中心に回転
+                if (count >= 1000) camera.LoRotateWithVec(origin, fk_Axis.Z, Math.PI/500.0);
 			}
 		}
 	}
