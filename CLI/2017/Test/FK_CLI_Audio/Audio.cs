32d4857b04bb9e94d448e4928757514d57b8b168
earth <earth@gamescience.jp>
2019-09-22 18:08:04 +0900

    190922

--- CLI/2017/Test/FK_CLI_Audio/Audio.cs
+++ CLI/2017/Test/FK_CLI_Audio/Audio.cs
@@ -1,92 +1,126 @@
 ﻿using System;
-using System.Threading.Tasks;
-using System.Threading;
 using FK_CLI;
 
 namespace FK_CLI_Audio
 {
-	class MyBGM : IDisposable
-	{
-		public bool endFlg { get; set; }
-		private fk_AudioStream bgm;
-		private bool openFlg;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // マテリアル初期化
+            fk_Material.InitDefault();
 
-		public MyBGM(string argFileName)
-		{
-			endFlg = false;
-			bgm = new fk_AudioStream();
-			openFlg = bgm.Open(argFileName);
-			if(openFlg == false) {
-				Console.WriteLine("Audio File Open Error.");
-			}
-		}
+            // ウィンドウの各種設定
+            var win = new fk_AppWindow();
+            WindowSetup(win);
 
-		public void Start()
-		{
-			if(openFlg == false) return;
-			bgm.LoopMode = true;
-			bgm.Gain = 0.5;
-			while(endFlg == false) {
-				bgm.Play();
-				Thread.Sleep(100);
-			}
-		}
+            // 立方体の各種設定
+            var blockModel = new fk_Model();
+            BlockSetup(blockModel, win);
 
-		public double Gain
-		{
-			set
-			{
-				bgm.Gain = value;
-			}
-		}
+            // BGM用変数の作成
+            var bgm = new fk_BGM("epoq.ogg");
+            double volume = 0.5;
 
-		public void Dispose()
-		{
-			bgm.Dispose();
-		}
-	}
+            // SE用変数の作成
+            var se = new fk_Sound(2);
+            // 音源読み込み (IDは0番)
+            se.LoadData(0, "MIDTOM2.wav");
+            // 音源読み込み (IDは1番)
+            se.LoadData(1, "SDCRKRM.wav");
 
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var win = new fk_AppWindow();
-			var block = new fk_Block(1.0, 1.0, 1.0);
-			var blockModel = new fk_Model();
-			var origin = new fk_Vector(0.0, 0.0, 0.0);
-			var audio = new MyBGM("epoq.ogg");
+            // ウィンドウ表示
+            win.Open();
 
-			double volume = 0.5;
+            // BGM再生スタート
+            bgm.Start();
 
-			fk_Material.InitDefault();			
-			blockModel.Shape = block;
-			blockModel.GlMoveTo(3.0, 3.0, 0.0);
-			blockModel.Material = fk_Material.Yellow;
-			win.Entry(blockModel);
-			win.CameraPos = new fk_Vector(0.0, 1.0, 20.0);
-			win.CameraFocus = new fk_Vector(0.0, 1.0, 0.0);
-			win.Size = new fk_Dimension(800, 600);
-			win.BGColor = new fk_Color(0.6, 0.7, 0.8);
-			win.Open();
-			win.ShowGuide(fk_GuideMode.GRID_XZ);
+            // SE出力スタンバイ
+            se.Start();
 
-			var bgmTask = new Task(audio.Start);
-			bgmTask.Start();
+            var origin = new fk_Vector(0.0, 0.0, 0.0);
+            while (win.Update())
+            {
+                blockModel.GlRotateWithVec(origin, fk_Axis.Y, Math.PI / 360.0);
 
-			while(win.Update()) {
-				blockModel.GlRotateWithVec(origin, fk_Axis.Y, Math.PI/360.0);
+                // volume値の変更
+                volume = ChangeVolume(volume, win);
+                bgm.Gain = volume;
 
-				if(win.GetKeyStatus('Z', fk_Switch.DOWN) == true && volume < 1.0) {
-					volume += 0.1;
-				}
-				if(win.GetKeyStatus('X', fk_Switch.DOWN) == true && volume > 0.0) {
-					volume -= 0.1;
-				}
+                // SE再生
+                PlaySE(se, win);
+            }
 
-				audio.Gain = volume;
-			}
-			audio.endFlg = true;
-			Task.WaitAll(new[] { bgmTask });
-		}
-	}
+            // BGM変数とSE変数に終了を指示
+            // (最後にこれをやらないといつまでも再生が止まらずプログラムが終了しません)
+            bgm.StopStatus = true;
+            se.StopStatus = true;
+        }
+
+        // ウィンドウ設定メソッド
+        static void WindowSetup(fk_AppWindow argWin)
+        {
+            argWin.CameraPos = new fk_Vector(0.0, 1.0, 20.0);
+            argWin.CameraFocus = new fk_Vector(0.0, 1.0, 0.0);
+            argWin.Size = new fk_Dimension(600, 600);
+            argWin.BGColor = new fk_Color(0.6, 0.7, 0.8);
+            argWin.ShowGuide(fk_GuideMode.GRID_XZ);
+            argWin.TrackBallMode = true;
+            argWin.FPS = 60;
+        }
+
+        // 立方体モデル設定メソッド
+        static void BlockSetup(fk_Model argModel, fk_AppWindow argWin)
+        {
+            // 立方体の各種設定
+            var block = new fk_Block(1.0, 1.0, 1.0);
+            argModel.Shape = block;
+            argModel.GlMoveTo(3.0, 3.0, 0.0);
+            argModel.Material = fk_Material.Yellow;
+            argWin.Entry(argModel);
+        }
+
+        // 音量調整メソッド
+        static double ChangeVolume(double argVol, fk_AppWindow argWin)
+        {
+            double tmpV = argVol;
+
+            // 上矢印キーで BGM 音量アップ
+            if (argWin.GetSpecialKeyStatus(fk_Key.UP, fk_Switch.DOWN) == true)
+            {
+                tmpV += 0.1;
+                if (tmpV > 1.0)
+                {
+                    tmpV = 1.0;
+                }
+            }
+
+            // 下矢印キーで BGM 音量ダウン
+            if (argWin.GetSpecialKeyStatus(fk_Key.DOWN, fk_Switch.DOWN) == true)
+            {
+                tmpV -= 0.1;
+                if (tmpV < 0.0)
+                {
+                    tmpV = 0.0;
+                }
+            }
+            return tmpV;
+        }
+
+        // SE再生(トリガー)メソッド
+        static void PlaySE(fk_Sound argSE, fk_AppWindow argWin)
+        {
+            // Z キーで 0 番の SE を再生開始
+            if (argWin.GetKeyStatus('Z', fk_Switch.DOWN) == true)
+            {
+                argSE.StartSound(0);
+            }
+
+            // X キーで 1 番の SE を再生開始
+            if (argWin.GetKeyStatus('X', fk_Switch.DOWN) == true)
+            {
+                argSE.StartSound(1);
+            }
+        }
+    }
 }
---
+++ CLI/FK_Audio/FK_Audio.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using FK_CLI;
+
+namespace FK_CLI
+{
+	// BGM用クラス
+	public class fk_BGM : IDisposable
+	{
+        private fk_AudioStream bgm;
+        private bool openStatus;
+        private Task task;
+
+        public bool StopStatus { get; set; }	// 終了指示用プロパティ
+
+		// コンストラクタ 引数は音源ファイル名 (Ogg 形式)
+		public fk_BGM(string argFileName)
+		{
+			StopStatus = false;
+			bgm = new fk_AudioStream();
+			openStatus = bgm.Open(argFileName);
+			if(openStatus == false) {
+				Console.WriteLine("Audio File Open Error.");
+			}
+            task = new Task(Start_);
+		}
+
+        private void Start_()
+        {
+			if(openStatus == false) return;
+			bgm.LoopMode = true;
+			bgm.Gain = 0.5;
+			while(StopStatus == false) {
+				bgm.Play();
+				Thread.Sleep(100);
+			}
+        }
+
+		// BGM再生処理
+		public void Start()
+		{
+            task.Start();
+        }
+
+        // 音量用プロパティ
+        public double Gain
+		{
+			set
+			{
+				bgm.Gain = value;
+			}
+		}
+
+		// スレッド終了時処理
+		public void Dispose()
+		{
+			bgm.Dispose();
+            Task.WaitAll(new[] { task });
+		}
+
+    }
+
+    // Sound Effect (SE) 用クラス
+    public class fk_Sound : IDisposable
+	{
+        private fk_AudioWavBuffer[] se;
+        private bool[] openStatus;
+        private bool[] playStatus;
+        private Task task;
+
+
+        public bool StopStatus { get; set; }	// 終了指示用プロパティ
+
+		// コンストラクタ 引数は音源の個数
+		public fk_Sound(int argNum)
+		{
+			StopStatus = false;
+			if(argNum < 1) return;
+			se = new fk_AudioWavBuffer [argNum];
+			openStatus = new bool [argNum];
+			playStatus = new bool [argNum];
+
+			for(int i = 0; i < argNum; i++)
+			{
+				se[i] = new fk_AudioWavBuffer();
+				openStatus[i] = false;
+				playStatus[i] = false;
+			}
+            task = new Task(Start_);
+		}
+
+		// SE音源読み込みメソッド (WAV 形式)
+		public bool LoadData(int argID, string argFileName)
+		{
+			if(argID < 0 || argID >= se.Length)
+			{
+				return false;
+			}
+
+			openStatus[argID] = se[argID].Open(argFileName);
+			if(openStatus[argID] == false)
+			{
+				Console.WriteLine("Audio File ({0}) Open Error.", argFileName);
+			}
+			se[argID].LoopMode = false;
+			se[argID].Gain = 0.5;
+			return true;
+		}
+
+		// SE開始メソッド
+		public void StartSound(int argID)
+		{
+			if(argID < 0 || argID >= se.Length) return;
+			playStatus[argID] = true;
+			se[argID].Seek(0.0);
+		}
+
+		// SE再生処理
+		private void Start_()
+		{
+			int i;
+
+			for(i = 0; i < se.Length; i++)
+			{
+				if(openStatus[i] == false) return;
+			}
+
+			while(StopStatus == false)
+			{
+				for(i = 0; i < se.Length; i++)
+				{
+					if(playStatus[i] == true)
+					{
+						playStatus[i] = se[i].Play();
+					}
+				}
+				Thread.Sleep(10);
+			}
+		}
+
+        public void Start()
+        {
+            task.Start();
+        }
+
+		// スレッド終了時処理
+		public void Dispose()
+		{
+			for(int i = 0; i < se.Length; i++)
+			{
+				se[i].Dispose();
+			}
+            Task.WaitAll(new[] { task });
+		}
+	}
+}
