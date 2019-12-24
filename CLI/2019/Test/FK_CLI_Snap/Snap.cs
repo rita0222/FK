244dcc7d2ec1419367523c8ad77b797889b3a486
earth <earth@gamescience.jp>
2019-12-24 10:36:38 +0900

    191224

--- CLI/2019/Test/FK_CLI_Snap/Snap.cs
+++ CLI/2019/Test/FK_CLI_Snap/Snap.cs
@@ -7,20 +7,15 @@ namespace FK_CLI_Snap
     {
         static void Main(string[] args)
         {
-            if(args.Length != 1)
+            if (args.Length != 1)
             {
-                Console.WriteLine("Max Num Input Error. Please Type Any Key.");
+                Console.WriteLine("Usage: {0} angle", Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine("Please Type Any Key.");
                 Console.ReadKey();
                 return;
             }
 
-            uint max = uint.Parse(args[0]);
-            if(max > 100)
-            {
-                Console.WriteLine("Number Error. Please Type Any Key.");
-                Console.ReadKey();
-                return;
-            }
+            int angle = int.Parse(args[0]);
 
             var block = new fk_Block(10.0, 20.0, 15.0);
             var model = new fk_Model();
@@ -34,6 +29,7 @@ namespace FK_CLI_Snap
 
             // モデルの色を黄色に設定
             model.Material = fk_Material.Yellow;
+            model.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.Y, angle * Math.PI / 180.0);
 
             // カメラの位置と方向を設定
             window.CameraPos = new fk_Vector(0.0, 0.0, 100.0);
@@ -49,19 +45,8 @@ namespace FK_CLI_Snap
             // ウィンドウを開く
             window.Open();
 
-            int num = 0;
-            for(int count = 0; window.Update(); count++)
-            {
-                // 直方体を Y 軸を中心に回転させる。
-                model.GlRotateWithVec(0.0, 0.0, 0.0, fk_Axis.Y, Math.PI / 500.0);
-
-                if(count % 10 == 0)
-                {
-                    window.SnapImage("image" + num.ToString("000") + ".bmp");
-                    num++;
-                    if (num == max) return;
-                }
-            }
+            window.Update();
+            window.SnapImage("image" + angle.ToString("0000") + ".png", fk_ImageType.PNG);
         }
     }
 }
