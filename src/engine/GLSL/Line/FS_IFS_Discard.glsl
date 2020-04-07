a1a9583c7c4706766ab9e8826aee252ba94c45a2
earth <earth@gamescience.jp>
2020-04-07 12:20:25 +0900

    200407

---
+++ src/engine/GLSL/Line/FS_IFS_Discard.glsl
@@ -0,0 +1,8 @@
+#version 410 core
+
+flat out vec4 put_color;
+
+void main()
+{
+	discard;
+}
---
+++ src/engine/GLSL/Line/FS_Line_Discard.glsl
@@ -0,0 +1,9 @@
+#version 410 core
+
+flat in vec4 put_color;
+flat in int draw_flag;
+
+void main()
+{
+	discard;
+}
