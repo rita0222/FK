5c6cdf88e74cc63663e67357485746e9bd94dc97
earth <earth@gamescience.jp>
2020-05-18 00:57:57 +0900

    200518

--- CLI/Installer/Core/core_copy.sh
+++ CLI/Installer/Core/core_copy.sh
@@ -6,13 +6,8 @@ COREDIR="netcoreapp3.1"
 OUTPATH="./FK_Core19"
 
 INPATH="../../Core2019/FK_Core_DLL"
-CORENAME="FK_Core.dll"
-
-AUDIOPATH="../../FK_Audio/bin"
-AUDIONAME="FK_Audio_Core.dll"
-
-FORMPATH="../../FK_FormHelper/bin"
-FORMNAME="FK_FormHelper_Core.dll"
+X86DIR="x86/Release"
+X64DIR="x64/Release"
 
 rm -rf $OUTPATH
 mkdir -p $OUTPATH/bin/x86
@@ -22,15 +17,11 @@ mkdir -p $OUTPATH/dll/x64
 mkdir $OUTPATH/doc
 mkdir $OUTPATH/redist
 
-cp $INPATH/Win32/Release/$COREDIR/$CORENAME $OUTPATH/bin/x86
-cp $AUDIOPATH/x86/Release/$COREDIR/$AUDIONAME $OUTPATH/bin/x86
-cp $FORMPATH/x86/Release/$COREDIR/$FORMNAME $OUTPATH/bin/x86
-cp $INPATH/Win32/Release/$COREDIR/Ijwhost.dll $OUTPATH/dll/x86
+cp $INPATH/$X86DIR/$COREDIR/FK_*.dll $OUTPATH/bin/x86
+cp $INPATH/$X86DIR/$COREDIR/Ijwhost.dll $OUTPATH/dll/x86
 
-cp $INPATH/x64/Release/$COREDIR/$CORENAME $OUTPATH/bin/x64
-cp $AUDIOPATH/x64/Release/$COREDIR/$AUDIONAME $OUTPATH/bin/x64
-cp $FORMPATH/x64/Release/$COREDIR/$FORMNAME $OUTPATH/bin/x64
-cp $INPATH/x64/Release/$COREDIR/Ijwhost.dll $OUTPATH/dll/x64
+cp $INPATH/$X64DIR/$COREDIR/FK_*.dll $OUTPATH/bin/x64
+cp $INPATH/$X64DIR/$COREDIR/Ijwhost.dll $OUTPATH/dll/x64
 
 cp ./doc/License.txt $OUTPATH/doc
 cp ./doc/Version_Core.txt $OUTPATH/doc/Version.txt
