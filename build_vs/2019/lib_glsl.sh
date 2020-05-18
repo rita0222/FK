9e4ec3dca4dd3cf2d86c244049803d025f490eab
earth <earth@gamescience.jp>
2020-05-18 22:49:29 +0900

    200518

--- build_vs/2019/lib_glsl.sh
+++ build_vs/2019/lib_glsl.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-SHADERDIR="../../src/shader/GLSL"
-GRAPHICSDIR="../../src/graphics/GLSL"
-
-touch $SHADERDIR/tmp.out
-rm $SHADERDIR/*.out
-$SHADERDIR/glsl2out $SHADERDIR/*.glsl
-
-touch $GRAPHICSDIR/tmp.out
-rm $GRAPHICSDIR/*.out
-$GRAPHICSDIR/glsl2out $GRAPHICSDIR/*.glsl
+perl ../../FK/glsl2out
